358a618a9801061296083bc1aefaf9d419bcdcf5
Alexis Toledo <alexis.toledo@live.com>
2021-10-18 09:32:45 +0200

    implements sass

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/index";
 import { getPokemons } from "./redux/action-creators/index";
+/* STYLES */
+import "./styles/main.sass";
 
 // Calls the API before rendering in order to feed the Redux state
 store.dispatch(getPokemons());
